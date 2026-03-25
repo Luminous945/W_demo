d21acdb51e0f240ac4970d65e08ba908a64ae8ea
Luminous945 <134146430+Luminous945@users.noreply.github.com>
2026-03-25 16:49:21 +0800

    fix:测试demo

---
+++ ThreadPool/main.cpp
@@ -0,0 +1,22 @@
+#include "ThreadPool.h"
+#include <iostream>
+
+
+void calc(int x, int y)
+{
+    int res = x + y;
+    cout << "res = " << res << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+}
+
+int main()
+{
+    ThreadPool pool(4);
+    for (int i = 0; i < 10; ++i)
+    {
+        auto func = bind(calc, i, i * 2);
+        pool.addTask(func);
+    }
+    getchar();
+    return 0;
+}
